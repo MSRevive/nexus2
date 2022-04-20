86eb0f363552274cb3112db56baffc4454bdabe5
Saint Wish <SaintWish@users.noreply.github.com>
2022-04-19 21:48:43 -0600

    remove the correct print this time

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -42,11 +42,10 @@ func setControlHeaders(w http.ResponseWriter) {
 
 func Log(next http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    log.Log.Debugf("Request received, processing %s %s from %s", r.Method, r.RequestURI, getIP(r))
     setControlHeaders(w) //best place to set control headers?
     start := time.Now()
     next.ServeHTTP(w, r)
-    //log.Log.Printf("%s %s from %s (%v)", r.Method, r.RequestURI, getIP(r), time.Since(start))
+    log.Log.Printf("%s %s from %s (%v)", r.Method, r.RequestURI, getIP(r), time.Since(start))
   })
 }
 
