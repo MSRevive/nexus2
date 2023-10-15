fb6bae5a1a7c23e978caf3ddf6156af595905849
Saint Wish <SaintWish@users.noreply.github.com>
2023-10-15 17:52:10 -0600

    wip: database schema

--- internal/database/schema/schema.go
+++ internal/database/schema/schema.go
@@ -1,5 +1,24 @@
 package schema
 
 import (
-	
+	"time"
 )
+
+type CharData struct {
+	Version int `bson:"version"`
+	CreatedAt time.Time `bson:"created_at"`
+	Size int `bson:"size"`
+	Data string `bson:"data"`
+}
+
+type Character struct {
+	Slot int `bson:"slot`
+	UpdatedAt time.Time `bson:"updated_at"`
+	DeletedAt time.Time `bson:"deleted_at"`
+	Versions map[int]CharData `bson:"data"` //Version => character data
+}
+
+type User struct {
+	ID string `bson:"_id"` //this is the SteamID64
+	Characters map[int]Character `bson:"characters"` //Slot => character
+}
