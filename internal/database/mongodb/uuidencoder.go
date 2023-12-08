05d9b382e99270d31b1e9a555b71753976ea2805
Saint Wish <SaintWish@users.noreply.github.com>
2023-12-07 18:56:35 -0700

    add mongodb encoder/decoder for UUID

---
+++ internal/database/mongodb/uuidencoder.go
@@ -0,0 +1,65 @@
+package mongodb
+
+//Taken from https://gist.github.com/SupaHam/3afe982dc75039356723600ccc91ff77
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsonrw"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+)
+
+var (
+	tUUID = reflect.TypeOf(uuid.UUID{})
+	uuidSubtype = byte(0x04)
+
+	mongoRegistry = bson.NewRegistryBuilder().
+		RegisterTypeEncoder(tUUID, bsoncodec.ValueEncoderFunc(uuidEncodeValue)).
+		RegisterTypeDecoder(tUUID, bsoncodec.ValueDecoderFunc(uuidDecodeValue)).
+		Build()
+)
+
+func uuidEncodeValue(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
+	if !val.IsValid() || val.Type() != tUUID {
+		return bsoncodec.ValueEncoderError{Name: "uuidEncodeValue", Types: []reflect.Type{tUUID}, Received: val}
+	}
+	b := val.Interface().(uuid.UUID)
+	return vw.WriteBinaryWithSubtype(b[:], uuidSubtype)
+}
+
+func uuidDecodeValue(dc bsoncodec.DecodeContext, vr bsonrw.ValueReader, val reflect.Value) error {
+	if !val.CanSet() || val.Type() != tUUID {
+		return bsoncodec.ValueDecoderError{Name: "uuidDecodeValue", Types: []reflect.Type{tUUID}, Received: val}
+	}
+
+	var data []byte
+	var subtype byte
+	var err error
+	switch vrType := vr.Type(); vrType {
+	case bsontype.Binary:
+		data, subtype, err = vr.ReadBinary()
+		if subtype != uuidSubtype {
+			return fmt.Errorf("unsupported binary subtype %v for UUID", subtype)
+		}
+	case bsontype.Null:
+		err = vr.ReadNull()
+	case bsontype.Undefined:
+		err = vr.ReadUndefined()
+	default:
+		return fmt.Errorf("cannot decode %v into a UUID", vrType)
+	}
+
+	if err != nil {
+		return err
+	}
+	uuid2, err := uuid.FromBytes(data)
+	if err != nil {
+		return err
+	}
+	val.Set(reflect.ValueOf(uuid2))
+	return nil
+}
