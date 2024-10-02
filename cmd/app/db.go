bc5d902810683675c03b77f71a57b126103aa22e
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2024-10-02 03:55:18 -0500

    run sync and gc in another thread

--- cmd/app/db.go
+++ cmd/app/db.go
@@ -37,25 +37,29 @@ func (a *App) SetupDatabase() error {
 func (a *App) SetupDatabaseAutoSave() {
 	syncCron := cron.New()
 	syncCron.AddFunc("*/30 * * * *", func(){ //This runs every 30 minutes.
-		a.Logger.Info("Syncing data to disk")
-		t1 := time.Now()
-		if err := a.DB.SyncToDisk(); err != nil {
-			a.Logger.Error("Failed to sync data to disk", "error", err)
-			return
-		}
-		a.Logger.Info("Finished syncing data to disk", "ping", time.Since(t1))
+		go func() {
+			a.Logger.Info("Syncing data to disk")
+			t1 := time.Now()
+			if err := a.DB.SyncToDisk(); err != nil {
+				a.Logger.Error("Failed to sync data to disk", "error", err)
+				return
+			}
+			a.Logger.Info("Finished syncing data to disk", "ping", time.Since(t1))
+		}()
 	})
 	syncCron.Start()
 
 	gcCron := cron.New()
 	gcCron.AddFunc("0 23 * * *", func(){ //This runs at 23:00 every day.
-		a.Logger.Info("Running database garbage collection")
-		t1 := time.Now()
-		if err := a.DB.RunGC(); err != nil {
-			a.Logger.Error("Failed to run garbage collection", "error", err)
-			return
-		}
-		a.Logger.Info("Finished running garbage collection", "ping", time.Since(t1))
+		go func() {
+			a.Logger.Info("Running database garbage collection")
+			t1 := time.Now()
+			if err := a.DB.RunGC(); err != nil {
+				a.Logger.Error("Failed to run garbage collection", "error", err)
+				return
+			}
+			a.Logger.Info("Finished running garbage collection", "ping", time.Since(t1))
+		}()
 	})
 	gcCron.Start()
 }
