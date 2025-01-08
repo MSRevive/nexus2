2b3935a71f2bdbf0fee4233ae0515c171f2aceea
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-01-08 11:45:08 -0600

    remove some badger options
    they don't make that much of a difference in our benchmarks like we would like.

--- internal/database/badger/badger.go
+++ internal/database/badger/badger.go
@@ -23,20 +23,6 @@ func New() *badgerDB {
 
 func (d *badgerDB) Connect(cfg database.Config, opts database.Options) error {
 	bOpts := badger.DefaultOptions(cfg.Badger.Directory)
-	bOpts.WithCompression(options.None)
-	bOpts.WithBlockSize(0)
-	bOpts.WithBlockCacheSize(0)
-	bOpts.WithMemTableSize(4 << 20)
-
-	// opts.MemTableSize = 1 << 20
-	// opts.BaseTableSize = 1 << 20
-	// //opts.NumCompactors = 2
-	// opts.NumLevelZeroTables = 1
-	// opts.NumLevelZeroTablesStall = 2
-	// opts.BlockCacheSize = 10 << 20
-	// //opts.NumMemtables = 1
-	// opts.ValueThreshold = 1 << 10
-	//bOpts.Logger = opts.Logger
 
 	db, err := badger.Open(bOpts)
 	if err != nil {
