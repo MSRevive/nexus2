73d5331e6a28ccc03747cce9b1970e713c3bceb5
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2026-04-12 01:53:25 -0500

    blackbox in case of catastrophic crash

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 	"os"
+	"runtime/debug"
 
 	"github.com/msrevive/nexus2/cmd"
 	"github.com/msrevive/nexus2/internal/static"
@@ -22,9 +23,23 @@ License: GPL-3.0 https://github.com/MSRevive/nexus2/blob/main/LICENSE %s
 `
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			crashLog(fmt.Errorf("panic: %v\n%s", r, debug.Stack()))
+			panic(r) // re-panic to preserve default behavior
+		}
+	}()
+
 	fmt.Printf(spMsg, time.Now().Year(), static.Version, "\n")
 
 	if err := cmd.Run(os.Args); err != nil {
+		crashLog(err)
 		panic(err)
 	}
+}
+
+func crashLog(err error) {
+	path := fmt.Sprintf("crash-%d.log", time.Now().Unix())
+	content := fmt.Sprintf("time: %s\nerror: %v\n", time.Now().Format(time.RFC3339), err)
+	_ = os.WriteFile(path, []byte(content), 0644)
 }
